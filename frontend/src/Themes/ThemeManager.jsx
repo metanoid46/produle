9d610dce588185232af7140a5131aab80a3a43fd
metanoid46 <athuljoseliju123@gmail.com>
2025-09-16 16:06:01 +1000

    everything updated, future updates for mobile screens will be done shortly

--- frontend/src/Themes/ThemeManager.jsx
+++ frontend/src/Themes/ThemeManager.jsx
@@ -1,15 +1,14 @@
 /* eslint-disable react-refresh/only-export-components */
 import React, { createContext, useMemo, useState, useEffect } from 'react';
 import { ConfigProvider } from 'antd';
-import { darkTheme, lightTheme } from './themes.js';
+import { darkTheme, lightTheme } from './themes.js'; // <-- added .js
 
-
+export const ThemeContext = createContext();
 
 const ThemeManager = ({ children }) => {
   const [isDarkMode, setIsDarkMode] = useState(false);
 
   const currentTheme = useMemo(() => (isDarkMode ? darkTheme : lightTheme), [isDarkMode]);
-
 
   useEffect(() => {
     document.documentElement.classList.toggle('dark-theme', isDarkMode);
@@ -25,6 +24,3 @@ const ThemeManager = ({ children }) => {
 };
 
 export default ThemeManager;
-
-
-export const ThemeContext = createContext();
